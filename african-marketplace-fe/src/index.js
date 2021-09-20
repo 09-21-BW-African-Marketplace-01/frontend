7d32426c95c05fca41f124149cea151a864e5ec9
christina-yun <poncheeyun@yahoo.com>
2021-09-20 15:46:05 -0700

    Built out the axiosWithAuth, PrivateRoute, and Login/Logout functions. Currently need to be able to connect those to backend so some of the code is left out

--- african-marketplace-fe/src/index.js
+++ african-marketplace-fe/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import reducer from './reducers/index'
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
@@ -12,15 +13,10 @@ import thunk from "redux-thunk";
 const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <Provider store={store}>
     <App />
     </Provider>
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
