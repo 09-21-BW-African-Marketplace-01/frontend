f7e36f860b2c40c0ee57ce2d5ab867818d015f79
christina-yun <poncheeyun@yahoo.com>
2021-09-21 12:48:14 -0700

    NavBar is finished for now

--- african-marketplace-fe/src/components/NavBar.js
+++ african-marketplace-fe/src/components/NavBar.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
-import { AppBar, Box, Toolbar, Typography, Button } from '@material-ui/core';
+import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
+
+import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
 
     return (
-    <nav>
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 6 }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             African Marketplace
           </Typography>
           <Button href='/profile' color="inherit">Profile</Button>
           <Button href='/login' color="inherit">Login</Button>
-          <Button href='logout' color="inherit">Logout</Button>
+          <Button href='/logout' color="inherit">Logout</Button>
         </Toolbar>
       </AppBar>
     </Box>
-    </nav>
-        
-        // <nav>
-        //     <h1>African Marketplace</h1>
-        //     <NavLink to='profile/'>Profile</NavLink>
-        //     <NavLink to='/login'>Login</NavLink>
-        //     <NavLink to='/logout'>Logout</NavLink>
-            
-       
-        // </nav>
         
     )
 }
